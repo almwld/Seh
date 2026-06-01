e515df057acff02d6ad40461af97db58e76c018c
almwld <almwld@github.com>
2026-06-01 21:46:22 +0300

    ✅ تبسيط main.dart - إزالة Firebase من sehatak_flutter

--- sehatak_flutter/lib/main.dart
+++ sehatak_flutter/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'presentation/screens/auth/splash_screen.dart';
 import 'core/themes/theme_manager.dart';
 import 'presentation/bloc/theme_bloc/theme_bloc.dart';
-import 'presentation/bloc/auth_bloc/auth_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,16 +11,6 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyAMcD8bz4fA9s5-MFXz2vlDErLVAdy8YfE',
-      appId: '1:302830226901:android:c768d699c649114216415c',
-      messagingSenderId: '302830226901',
-      projectId: 'sehatak-latform',
-      storageBucket: 'sehatak-latform.firebasestorage.app',
-    ),
-  );
 
   runApp(const MyApp());
 }
@@ -33,16 +21,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => AuthBloc()..add(AppStarted()),
+      create: (_) => ThemeBloc(),
       child: BlocBuilder<ThemeBloc, ThemeState>(
         builder: (context, state) {
           return MaterialApp(
             title: 'صحتك',
             debugShowCheckedModeBanner: false,
-            builder: (context, child) => Directionality(
-              textDirection: TextDirection.rtl,
-              child: child!,
-            ),
+            builder: (context, child) {
+              return Directionality(
+                textDirection: TextDirection.rtl,
+                child: child!,
+              );
+            },
             theme: ThemeManager.lightTheme,
             darkTheme: ThemeManager.darkTheme,
             themeMode: state is ThemeLoadedState ? state.themeMode : ThemeMode.light,
